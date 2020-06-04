4c52a2fb8b1d27cb49dcc9159c38f0bcb90db363
Marc DiPasquale <Marc.DiPasquale@solace.com>
2020-06-04 14:50:33 -0400

    Add a second consumer & enable direct messaging

--- GoogleToSolaceBridge/src/main/java/org/taxi/nyc/Application.java
+++ GoogleToSolaceBridge/src/main/java/org/taxi/nyc/Application.java
@@ -4,6 +4,8 @@ package org.taxi.nyc;
 import java.util.function.Consumer;
 
 import javax.annotation.PostConstruct;
+import javax.jms.ConnectionFactory;
+import javax.jms.DeliveryMode;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -12,6 +14,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.jms.JmsException;
+import org.springframework.jms.config.DefaultJmsListenerContainerFactory;
 import org.springframework.jms.connection.CachingConnectionFactory;
 import org.springframework.jms.core.JmsTemplate;
 
@@ -25,6 +28,9 @@ public class Application {
 
 	@Autowired
 	private JmsTemplate jmsTemplate = null;
+
+	@Autowired
+	private JmsTemplate jmsTemplate2 = null;
 	private ObjectMapper mapper = new ObjectMapper();
 	private String topicPrefix = "taxi/nyc/v1/";
 
@@ -49,7 +55,36 @@ public class Application {
 					+ latitude;
 			try {
 				jmsTemplate.convertAndSend(topic, mapper.writeValueAsString(input));
-				logger.info("Sending "+input+" to topic:"+topic);
+				logger.info("Sending " + input + " to topic:" + topic);
+			} catch (JmsException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (JsonProcessingException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+
+		};
+	}
+
+	@Bean
+	public Consumer<TaxiStatusUpdatePayload> bridgeGcpToSolace2() {
+		return input -> {
+			Double latitude = input.getLatitude();
+			Double longitude = input.getLongitude();
+			Double meterIncrement = input.getMeterIncrement();
+			Double meterReading = input.getMeterReading();
+			Integer passengerCount = input.getPassengerCount();
+			Integer pointIdx = input.getPointIdx();
+			String rideId = input.getRideId();
+			String rideStatus = input.getRideStatus();
+			String timestamp = input.getTimestamp();
+
+			String topic = topicPrefix + rideStatus + "/" + passengerCount + "/" + rideId + "/" + longitude + "/"
+					+ latitude;
+			try {
+				jmsTemplate2.convertAndSend(topic, mapper.writeValueAsString(input));
+//				logger.info("Sending "+input+" to topic:"+topic);
 			} catch (JmsException e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
@@ -60,15 +95,34 @@ public class Application {
 			
 		};
 	}
-	
+
 	@PostConstruct
 	private void fixJMSTemplate() {
 		// Code that makes the JMS Template Cache Connections for Performance.
 		CachingConnectionFactory ccf = new CachingConnectionFactory();
-		ccf.setTargetConnectionFactory(jmsTemplate.getConnectionFactory());
+		ConnectionFactory cf = jmsTemplate.getConnectionFactory();
+		ccf.setTargetConnectionFactory(cf);
 		jmsTemplate.setConnectionFactory(ccf);
+		jmsTemplate.setExplicitQosEnabled(true);
 		jmsTemplate.setPubSubDomain(true);
-		jmsTemplate.setTimeToLive(24*60*60*1000);
+		jmsTemplate.setTimeToLive(24 * 60 * 60 * 1000);
+		jmsTemplate.setDeliveryPersistent(false);
+		jmsTemplate.setDeliveryMode(DeliveryMode.NON_PERSISTENT);
+		jmsTemplate.setPriority(0);
 	}
+	
 
+	@PostConstruct
+	private void fixJMSTemplate2() {
+		// Code that makes the JMS Template Cache Connections for Performance.
+		CachingConnectionFactory ccf = new CachingConnectionFactory();
+		ccf.setTargetConnectionFactory(jmsTemplate2.getConnectionFactory());
+		jmsTemplate2.setConnectionFactory(ccf);
+		jmsTemplate2.setExplicitQosEnabled(true);
+		jmsTemplate2.setPubSubDomain(true);
+		jmsTemplate2.setTimeToLive(24 * 60 * 60 * 1000);
+		jmsTemplate2.setDeliveryPersistent(false);
+		jmsTemplate2.setDeliveryMode(DeliveryMode.NON_PERSISTENT);
+		jmsTemplate2.setPriority(0);
+	}
 }
