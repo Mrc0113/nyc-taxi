e245a09d8b9b4a18d9b5c0707516a3e2a159ced8
Marc DiPasquale <Marc.DiPasquale@solace.com>
2020-06-04 10:29:43 -0400

    Initial

---
+++ GoogleToSolaceBridge/README.md
@@ -0,0 +1,6 @@
+# GoogleToSolaceBridge
+
+## Version 0.0.1
+
+<p>This application is a Spring Cloud Stream application that receives NYC taxi events from GCP and republishes them into a Solace Event Mesh. </p>
+
---
+++ GoogleToSolaceBridge/src/main/java/org/taxi/nyc/Application.java
@@ -0,0 +1,42 @@
+
+package org.taxi.nyc;
+
+
+import java.util.function.Consumer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.stream.function.StreamBridge;
+import org.springframework.context.annotation.Bean;
+
+
+@SpringBootApplication
+public class Application {
+
+	private static final Logger logger = LoggerFactory.getLogger(Application.class);
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class);
+	}
+
+	@Bean
+	public Consumer<TaxiStatusUpdatePayload> bridgeGcpToSolace(StreamBridge streamBridge) {
+		return input -> {
+		    Double latitude = input.getLatitude();
+		    Double longitude = input.getLongitude();
+		    Double meterIncrement = input.getMeterIncrement();
+		    Double meterReading = input.getMeterReading();
+		    Integer passengerCount = input.getPassengerCount();
+		    Integer pointIdx = input.getPointIdx();
+		    String rideId = input.getRideId();
+		    String rideStatus = input.getRideStatus();
+		    String timestamp = input.getTimestamp();
+		    
+			String topic = "nyc/taxi/v1/"+rideStatus+"/"+passengerCount+"/"+rideId+"/"+longitude+"/"+latitude;  
+			streamBridge.send(topic, input);
+		};
+	}
+
+}
